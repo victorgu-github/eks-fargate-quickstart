ef7f20fb2daced2b5a90efdfb32e5cefabf64acc
Gu <victorgw@amazon.com>
2022-10-03 09:18:52 -0500

    update after lab

--- eks-fargate-quickstart/base.tfvars
+++
@@ -1,5 +0,0 @@
-name   = "aws007-preprod-test-eks"
-region = "us-east-2"
-vpc_cidr = "10.0.0.0/16"
-opensearch_dashboard_user = "aws_demo"
-opensearch_dashboard_pw = "AWSDemo123!"
--- terraform-aws-eks-blueprints/modules/irsa/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "irsa_iam_role_arn" {
-  description = "IAM role ARN for your service account"
-  value       = var.irsa_iam_policies != null ? aws_iam_role.irsa[0].arn : null
-}
-
-output "irsa_iam_role_name" {
-  description = "IAM role name for your service account"
-  value       = var.irsa_iam_policies != null ? aws_iam_role.irsa[0].name : null
-}
